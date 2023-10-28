15339da0023eb287989a6b229af6416592377c70
leedy903 <leedy903@naver.com>
2023-10-29 05:17:26 +0900

    refactor: 경주 클래스 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,76 +1,12 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 public class Application {
     private static List<Car> cars;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
-        String userInputCarNames = userInput();
-        List<String> carNames = Arrays.stream(userInputCarNames.split(",")).toList();
-
-        cars = new ArrayList<Car>();
-        for (String carName: carNames) {
-            Car car = new Car(carName);
-            cars.add(car);
-        }
-
-        int userInputNumberOfAttempts = Integer.parseInt(userInput());
-
-        for (int i = 0; i < userInputNumberOfAttempts; i++) {
-            goForwardRandomCar();
-            showRaceResult();
-        }
-
-        showFinalWinners();
-
-        Console.close();
-    }
-
-    private static String userInput() {
-        String userInput = Console.readLine();
-        return userInput;
-    }
-
-    private static void goForwardRandomCar() {
-        for (int i = 0; i < cars.size(); i++) {
-            int randomNumber = Randoms.pickNumberInRange(0, 9);
-            System.out.println(i + cars.get(i).getName() + randomNumber);
-            if (randomNumber >= 4) {
-                cars.get(i).goForward();
-            }
-        }
-    }
-
-    private static void showRaceResult() {
-        for (int i = 0; i < cars.size(); i++) {
-            cars.get(i).showResult();
-        }
-    }
-
-    private static void showFinalWinners() {
-        List<String> winners = new ArrayList<String>();
-
-        int maxDistance = 0;
-
-        for(int i = 0; i < cars.size(); i++) {
-            maxDistance = Math.max(maxDistance, cars.get(i).getDrivingDistance());
-        }
-
-        for (int i = 0; i < cars.size(); i++) {
-            if (maxDistance == cars.get(i).getDrivingDistance()) {
-                winners.add(cars.get(i).getName());
-            }
-        }
-
-        String result = String.join(", ", winners);
-        System.out.println("최종 우승자 : " + result);
+        Game game = new Game();
+        game.run();
     }
 }
---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,86 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Game {
+    private List<Car> cars;
+    private int rounds = 0;
+
+    public Game() {
+    }
+
+    public void run() {
+        initCars();
+        initRounds();
+        playRace();
+        showFinalWinners();
+        Console.close();
+    }
+
+    private void initCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String userInputCarNames = userInput();
+        List<String> carNames = Arrays.stream(userInputCarNames.split(",")).toList();
+        cars = new ArrayList<Car>();
+        for (String name: carNames) {
+            Car car = new Car(name);
+            cars.add(car);
+        }
+    }
+
+    private void initRounds() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        rounds = Integer.parseInt(userInput());
+    }
+
+
+    private String userInput() {
+        String userInput = Console.readLine();
+        return userInput;
+    }
+
+    private void playRace() {
+        for (int i = 0; i < rounds; i++) {
+            playOneRound();
+            showRaceCondition();
+        }
+    }
+
+    private void playOneRound() {
+        for (int i = 0; i < cars.size(); i++) {
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+            if (randomNumber >= 4) {
+                cars.get(i).goForward();
+            }
+        }
+    }
+
+    private void showRaceCondition() {
+        for (int i = 0; i < cars.size(); i++) {
+            cars.get(i).showResult();
+        }
+    }
+
+    private void showFinalWinners() {
+        List<String> winners = new ArrayList<String>();
+
+        int maxDistance = 0;
+
+        for(int i = 0; i < cars.size(); i++) {
+            maxDistance = Math.max(maxDistance, cars.get(i).getDrivingDistance());
+        }
+
+        for (int i = 0; i < cars.size(); i++) {
+            if (maxDistance == cars.get(i).getDrivingDistance()) {
+                winners.add(cars.get(i).getName());
+            }
+        }
+
+        String result = String.join(", ", winners);
+        System.out.println("최종 우승자 : " + result);
+    }
+}
